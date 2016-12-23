504f4b83bee1719b8e62e052b612d3f26053fca5
Andreas <andreas.schmueckert@webvariants.de>
2016-12-23 06:06:04 +0100

    initial commit

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent implements OnInit {
   // Link to our api, pointing to localhost
   API = 'http://localhost:3000';
 
-  // Declare empty list of people
-  people: any[] = [];
+  // Declare empty list of messages
+  messages: any[] = [];
 
   constructor(private http: Http) {}
 
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit {
     this.getAllMessages();
   }
 
-  // Add one person to the API
+  // Add one message to the API
   addMessage(fLid, driverId) {
     const endTime = Date.now();
     const startTime = endTime - 86400000; // remove one day from the end time to set start time
@@ -34,11 +34,11 @@ export class AppComponent implements OnInit {
     this.http.post(`${this.API}/messages`, {fLid, driverId, startDate, endDate})
       .map(res => res.json())
       .subscribe(() => {
-        this.getAllPeople();
+        this.getAllMessages();
       }, error => console.log(error))
   }
 
-  // Get all users from the API
+  // Get all messages from the API
   getAllMessages() {
     this.http.get(`${this.API}/messages`)
       .map(res => res.json())
